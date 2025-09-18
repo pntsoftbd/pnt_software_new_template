1ed7c88ff8cd08baaff0a14ea387f4231dcdf6b7
Sheik Mostafizur <sheikmostafizur2001@gmail.com>
2025-09-18 18:29:43 +0600

    feat(ui): add shadcn/ui and configure colors

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1 className="bg-primary">Home page!</h1>
+        <h1 className="bg-secondary">Home page!</h1>
+      </header>
+    </>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Hello World!</h1>
-      <p className="font-manrope">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores reiciendis voluptatem asperiores voluptate alias laboriosam tempore totam quis, aliquam, explicabo fugit aspernatur accusamus cupiditate quasi, maxime eum omnis ullam beatae?</p>
-    </main>
-  );
-}
